6d5ba7d6f9bbfdfed495de8c03005c3feec19d91
AlexCFrost <siva.ganesh.c18@gmail.com>
2023-12-12 16:32:24 +0530

    Finished

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import './App.css'
+import useStorage from "./useStorage";
+
+function App() {
+  const [localName, setLocalName] = useStorage("localName", "");
+  const [sessionName, setSessionName] = useStorage("sessionName", "", sessionStorage);
+
+  return (
+    <>
+      <div>
+        <label>Local Storage:</label>
+        <input
+          type="text"
+          value={localName}
+          onChange={(e) => setLocalName(e.target.value)}
+        />
+      </div>
+      <div>
+        <label>Session Storage:</label>
+        <input
+          type="text"
+          value={sessionName}
+          onChange={(e) => setSessionName(e.target.value)}
+        />
+      </div>
+    </>
+  );
+}
+
+export default App;
+
+
+
